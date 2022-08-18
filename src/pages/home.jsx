c555c6a7351bf2e484d5fa2c1afa9464f3064b37
Carter Cobb <carter.cobb72@gmail.com>
2022-08-18 09:37:25 -0600

    styled home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,9 @@ import Login from "../components/login";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-container">
+      <h1>Email Search Tool</h1>
+      <p>Please login using your Gmail account:</p>
       <Login />
     </div>
   );
