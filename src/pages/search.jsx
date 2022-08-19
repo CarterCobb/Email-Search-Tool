f4286ecc2ca392b565df569038fae3efddbb8c52
Carter Cobb <carter.cobb72@gmail.com>
2022-08-18 23:08:56 -0600

    implemeted page

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import EmailTable from "../components/email-table";
 import Logout from "../components/logout";
 import useEmails from "../hooks/useEmails";
 
@@ -15,25 +16,74 @@ const SearchPage = ({ g_user, g_auth }) => {
     emails.search(search);
   };
 
+  const styles = {
+    searchContainer: {
+      float: "left",
+      disply: "flex",
+      padding: "20px",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      flexDirection: "row",
+    },
+  };
+
   return (
     <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          color: "black",
+        }}
+      >
+        <h1 style={{ color: "black", padding: 20, fontSize: 30 }}>
+          Welcome {g_user.name}!
+        </h1>
+        <img src={g_user.image} alt="account" />
+        <p style={{ color: "black", padding: 15, fontSize: 18 }}>
+          Inbox for: {g_user.email}
+        </p>
+      </div>
       <Logout />
-      <pre>{JSON.stringify(g_user, null, 2)}</pre>
-      <pre>{JSON.stringify(g_auth, null, 2)}</pre>
-      <form onSubmit={onSearch}>
-        <input
-          value={search}
-          onChange={({ target: { value } }) => setSearch(value)}
-          placeholder="Search"
-        />
-        <input type="submit" value="Search" />
-      </form>
-      <p>Next Page ID: {emails.nextPageId}</p>
-      {emails.loading ? (
-        <h1>Loading Emails...</h1>
-      ) : (
-        <pre>{JSON.stringify(emails.list, null, 2)}</pre>
-      )}
+      <div
+        style={{
+          float: "left",
+          display: "flex",
+          padding: 20,
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexDirection: "row",
+        }}
+      >
+        <form onSubmit={onSearch}>
+          <input
+            value={search}
+            onChange={({ target: { value } }) => setSearch(value)}
+            placeholder="Search"
+            className="search-bar"
+          />
+          <input type="submit" value="Search" className="page-btn" />
+          {emails.nextPageId && (
+            <input
+              type="button"
+              className="page-btn"
+              onClick={() => emails.requestAll(emails.nextPageId)}
+              value="Next Page"
+            />
+          )}
+        </form>
+        <span>
+          Loaded: {emails.loaded}/{emails.count} (
+          {(emails.loaded / emails.count) * 100}%)
+        </span>
+      </div>
+      <EmailTable
+        emails={emails.list}
+        loading={emails.loading}
+        search={search}
+      />
     </div>
   );
 };
